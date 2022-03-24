a2421ebd86a6da691a131cb33b0bc36babcaa5d6
ZserboZsiraf <lakanoda@gmail.com>
2022-03-24 19:47:39 +0100

    Equipment abstract osztály

--- src/team_qgli/Equipment.java
+++ src/team_qgli/Equipment.java
@@ -2,7 +2,7 @@ package team_qgli;
 
 public abstract class Equipment implements Collectible {
     @Override
-    public void Collect(Virologist v) {}
+    public abstract void Collect(Virologist v);
     public void Action(Virologist v){}
     public void Remove(Virologist v){}
 }
