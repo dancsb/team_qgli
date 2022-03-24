52dfb0f1fb972689e7b707a7b597afa45cf2e06d
ZserboZsiraf <lakanoda@gmail.com>
2022-03-24 19:54:46 +0100

    Resources leszáramozottjai üresen

---
+++ src/team_qgli/AminoAcid.java
@@ -0,0 +1,13 @@
+package team_qgli;
+
+public class AminoAcid extends Resources{
+    @Override
+    public void Collect(Virologist v) {
+        super.Collect(v);
+    }
+
+    @Override
+    public void ChangeAmountAminoAcid(long x) {
+        super.ChangeAmountAminoAcid(x);
+    }
+}
---
+++ src/team_qgli/Nucleotide.java
@@ -0,0 +1,13 @@
+package team_qgli;
+
+public class Nucleotide extends Resources{
+    @Override
+    public void ChangeAmountNucleotide(long x) {
+        super.ChangeAmountNucleotide(x);
+    }
+
+    @Override
+    public void Collect(Virologist v) {
+        super.Collect(v);
+    }
+}
