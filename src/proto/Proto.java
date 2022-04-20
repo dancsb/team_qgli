824bc360fedf37b0aa43504f8eccdc517fa0fafc
ZserboZsiraf <lakanoda@gmail.com>
2022-04-21 01:33:09 +0200

    Proto package+osztály létrehozva

---
+++ src/proto/Proto.java
@@ -0,0 +1,12 @@
+package proto;
+
+import main.virologist.Virologist;
+
+import java.util.HashMap;
+
+public class Proto {
+    /**
+     * Eltároljuk benne a virológusokat. Az azonosító a key.
+     */
+    private HashMap<String, Virologist>virologists;
+}
