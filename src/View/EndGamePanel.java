1e0ec765da7d83aa1de362ee180844a7bf9cff37
Ábel Fosztó-Bartha <abel.foszto-bartha@edu.bme.hu>
2022-05-14 21:27:01 +0200

    medveWin GUI

--- src/View/EndGamePanel.java
+++ src/View/EndGamePanel.java
@@ -13,6 +13,16 @@ public class EndGamePanel extends JPanel {
 
     public EndGamePanel(int currentVirologist){
         this.setPreferredSize(new Dimension(800,500));
+
+        if(currentVirologist==-1){
+            try {
+                virologistImage = (ImageIO.read(new File("src/img/bear.png"))).getScaledInstance(384,384,Image.SCALE_DEFAULT);
+                winnerImage =  (ImageIO.read(new File("src/img/winner.png"))).getScaledInstance(400,160,Image.SCALE_DEFAULT);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        else
         try {
             virologistImage = (ImageIO.read(new File("src/img/virologist_" +(currentVirologist+1)+ ".png"))).getScaledInstance(384,384,Image.SCALE_DEFAULT);
             winnerImage =  (ImageIO.read(new File("src/img/winner.png"))).getScaledInstance(400,160,Image.SCALE_DEFAULT);
