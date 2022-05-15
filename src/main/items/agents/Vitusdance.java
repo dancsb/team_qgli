a48cfd6c25d716074ce3a6fb91fb728aea874fe4
ZserboZsiraf <lakanoda@gmail.com>
2022-05-15 22:29:33 +0200

    vitusdance javítva

--- src/main/items/agents/Vitusdance.java
+++ src/main/items/agents/Vitusdance.java
@@ -24,7 +24,7 @@ public class Vitusdance extends Agent {
      * @param v a virologus amelyen az agenst hasznaljuk
      */
     public void useOn(Virologist v) {
-        v.getAttributes().forEach(a->a.setTimeDance(expireTime));
+        v.getAttributes().forEach(a->a.setTimeDance(effectTime));
     }
     @Override
     public void printStat(){
